a07fdccf2cb4578d2857ff3cba887c474881c00f
Honour <nosakhare073@gmail.com>
2023-09-10 09:16:28 +0100

    fix: add allowed hosts

--- hng_test_one/settings.py
+++ hng_test_one/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^&2#^o@8cumkbw*29h3&1p2nqx8qluyljhwa(+cj@ih418vnwt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-testing-production.up.railway.app']
 
 
 # Application definition
