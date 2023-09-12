07f6778307d5f5c1dc48900c55ecffcc10a933f7
Honour <nosakhare073@gmail.com>
2023-09-12 20:45:59 +0100

    fix: optional description field

--- hng_test_two/models.py
+++ hng_test_two/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Person(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=40)
-    description = models.CharField(max_length=100, blank=True, null=True)
+    description = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return self.name
